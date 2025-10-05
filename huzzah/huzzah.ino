57896bc5515d85c33e0cc27efc2090783ec2b436
Eldor Bekpulatov <eldorbek0526@gmaail.com>
2025-10-05 09:31:39 -0700

    Added driver code for VCNL and BML sensors.

--- huzzah/huzzah.ino
+++ huzzah/huzzah.ino
@@ -1,12 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
+#include <Adafruit_ICM20X.h>
+#include <Adafruit_ICM20948.h>
 #include <Adafruit_VCNL4200.h>
 #include <Adafruit_VoltageSens.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
 // Global structs
+Adafruit_ICM20948 icm;
 Adafruit_VCNL4200 vcnl;
+Adafruit_BMP3XX bmp;
 Adafruit_VoltageSens volt;
 AsyncServer *server = nullptr;
+
+uint16_t measurement_delay_us = 65535; // Delay between measurements for testing
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 // 🔐 Wi-Fi credentials
 const char* ssid = "TMOBILE-6BD8";
@@ -76,6 +85,96 @@ void initialize_wifi() {
   Serial.println("🚀 Async TCP server started on port 80");
 }
 
+void initialize_icm20948(){
+  // Try to initialize!
+  if (!icm.begin_I2C()) {
+    // if (!icm.begin_SPI(ICM_CS)) {
+    // if (!icm.begin_SPI(ICM_CS, ICM_SCK, ICM_MISO, ICM_MOSI)) {
+
+    Serial.println("Failed to find ICM20948 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("ICM20948 Found!");
+  // icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (icm.getAccelRange()) {
+  case ICM20948_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case ICM20948_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case ICM20948_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case ICM20948_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  Serial.println("OK");
+
+  // icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (icm.getGyroRange()) {
+  case ICM20948_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  }
+
+  //  icm.setAccelRateDivisor(4095);
+  uint16_t accel_divisor = icm.getAccelRateDivisor();
+  float accel_rate = 1125 / (1.0 + accel_divisor);
+
+  Serial.print("Accelerometer data rate divisor set to: ");
+  Serial.println(accel_divisor);
+  Serial.print("Accelerometer data rate (Hz) is approximately: ");
+  Serial.println(accel_rate);
+
+  //  icm.setGyroRateDivisor(255);
+  uint8_t gyro_divisor = icm.getGyroRateDivisor();
+  float gyro_rate = 1100 / (1.0 + gyro_divisor);
+
+  Serial.print("Gyro data rate divisor set to: ");
+  Serial.println(gyro_divisor);
+  Serial.print("Gyro data rate (Hz) is approximately: ");
+  Serial.println(gyro_rate);
+
+  // icm.setMagDataRate(AK09916_MAG_DATARATE_10_HZ);
+  Serial.print("Magnetometer data rate set to: ");
+  switch (icm.getMagDataRate()) {
+  case AK09916_MAG_DATARATE_SHUTDOWN:
+    Serial.println("Shutdown");
+    break;
+  case AK09916_MAG_DATARATE_SINGLE:
+    Serial.println("Single/One shot");
+    break;
+  case AK09916_MAG_DATARATE_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case AK09916_MAG_DATARATE_20_HZ:
+    Serial.println("20 Hz");
+    break;
+  case AK09916_MAG_DATARATE_50_HZ:
+    Serial.println("50 Hz");
+    break;
+  case AK09916_MAG_DATARATE_100_HZ:
+    Serial.println("100 Hz");
+    break;
+  }
+
+  Serial.println("Adafruit ICM-20948 initialized successfully!");
+}
 
 void initialize_vcnl4200(){
   if (!vcnl.begin()) {
@@ -106,17 +205,53 @@ void initialize_voltage_sensor(){
   Serial.println("Voltage Sensor initialized!");
 }
 
+void initialize_bmp388(){
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
 
-
-void print_vcnl_readings( uint16_t proxData, uint16_t alsData, uint16_t whiteData){
-  Serial.print("Prox Data: ");
-  Serial.print(proxData);
-  Serial.print(", ALS Data: ");
-  Serial.print(alsData);
-  Serial.print(", White Data: ");
-  Serial.println(whiteData);
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
+void print_icm_readings(sensors_event_t accel, sensors_event_t gyro, sensors_event_t temp, sensors_event_t mag){
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  Serial.print("\t\tMag X: ");
+  Serial.print(mag.magnetic.x);
+  Serial.print(" \tY: ");
+  Serial.print(mag.magnetic.y);
+  Serial.print(" \tZ: ");
+  Serial.print(mag.magnetic.z);
+  Serial.println(" uT");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
+}
 
 void print_vcnl_readings( uint16_t proxData, uint16_t alsData, uint16_t whiteData){
   Serial.print("Prox Data: ");
@@ -131,6 +266,20 @@ void print_voltage_reading(sensors_event_t voltage_event){
   Serial.print("Voltage: ");
   Serial.print(voltage_event.voltage);
   Serial.println(" V");
+}
+
+void print_bmp388_readings(){
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure() / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 }
 
 // volatile int counter = 0;
@@ -154,7 +303,8 @@ void setup(void) {
   initialize_wifi();
   initialize_vcnl4200();
   initialize_voltage_sensor();
-  calibrate_mpu_6050();
+  initialize_icm20948();
+  initialize_bmp388();
 
   Serial.println("");
   delay(100); // blocking
@@ -167,6 +317,18 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
+    
+    /* TODO: maybe rewrite some of these functions*/
+    bmp.performReading();
+    print_bmp388_readings();
+
+    /* Get a new normalized sensor event */
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t mag;
+    sensors_event_t temp;
+    icm.getEvent(&accel, &gyro, &temp, &mag);
+    print_icm_readings(accel, gyro, temp, mag);
     
     // Read the proximity sensor data
     uint16_t proxData = vcnl.readProxData();
