0de5fac1e87383244602379f03c2937502d7a8c4
Eldor Bekpulatov <eldorbek0526@gmaail.com>
2026-01-26 23:27:31 -0800

    Added 2 methods of calculating pitch and roll angles

--- qtpy/qtpy.ino
+++ qtpy/qtpy.ino
@@ -96,10 +96,15 @@ void initialize_icm20948(){
     while (1) { delay(10); }
   }
   icm.setAccelRange(ICM20948_ACCEL_RANGE_2_G);
-  icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
-  icm.setAccelRateDivisor(4095);
-  icm.setGyroRateDivisor(255);
+  icm.setGyroRange(ICM20948_GYRO_RANGE_250_DPS);
+  
+  icm.enableAccelDLPF(true, ICM20X_ACCEL_FREQ_5_7_HZ);
+  icm.enableGyrolDLPF(true, ICM20X_GYRO_FREQ_5_7_HZ);
+ 
+  icm.setAccelRateDivisor(10); // 102.3 Hz
+  icm.setGyroRateDivisor(10); // 102.3 Hz
   icm.setMagDataRate(AK09916_MAG_DATARATE_100_HZ);
+
   Serial.println("Adafruit ICM-20948 initialized successfully!");
 }
 
@@ -155,6 +160,27 @@ void print(sensors_event_t *accel, sensors_event_t *gyro, sensors_event_t *mag,
 // pinMode(0, OUTPUT); // PWM
 // analogWrite(0, 127); 
 
+
+float gyro_bias_x = 0.0f;
+float gyro_bias_y = 0.0f;
+float gyro_bias_z = 0.0f;
+
+void calibrate_icm() {
+    const int N = 50;
+    sensors_event_t a, g, m, t;
+    for (int i = 0; i < N; i++) {
+        icm.getEvent(&a, &g, &t, &m);
+        gyro_bias_x += g.gyro.x;
+        gyro_bias_y += g.gyro.y;
+        gyro_bias_z += g.gyro.z;
+        delay(10);
+    }
+    gyro_bias_x /= N;
+    gyro_bias_y /= N;
+    gyro_bias_z /= N;
+}
+
+
 void setup(void) {
   Serial.begin(115200);
   while (!Serial){ delay(10); }
@@ -168,28 +194,81 @@ void setup(void) {
   initialize_bmp388();
   initialize_icm20948();
 
+  calibrate_icm();
+
   Serial.println("");
   delay(100); // blocking
 }
 
-// // Non-blocking loop using millis() for timing
-// unsigned long previousMillis = 0; 
-// const long interval = 500;
-// void loop() {
-//   unsigned long currentMillis = millis();
-//   if (currentMillis - previousMillis >= interval) {
-//     previousMillis = currentMillis;
-//     // ... do something every 500ms
-//   }
-// }
+float prev_roll_rad = 0.0f;
+float prev_pitch_rad = 0.0f;
 
-void loop() {
-  // sensor reads take about 8 milis 
-  sensors_event_t press, alt, temp;
-  bmp.getEvent(&temp, &press, &alt);
-  sensors_event_t accel, gyro, mag, temp2;
-  icm.getEvent(&accel, &gyro, &temp2, &mag);
-
-  print(&accel, &gyro, &mag, &temp2, &press, &alt);
-  delay(500); // blocking
+/* Returns integrated roll angle in degreed */
+float integrate_roll(sensors_event_t* gyro) {
+  float dt = 0.020f;  // 20 ms
+  prev_roll_rad += (gyro->gyro.y) * dt;
+  return degrees(prev_roll_rad);
 }
+
+/* Returns integrated pitch angle in degrees */
+float integrate_pitch(sensors_event_t* gyro) {
+  float dt = 0.020f;  // 20 ms
+  prev_pitch_rad += (gyro->gyro.x) * dt;
+  return degrees(prev_pitch_rad);
+}
+
+/* Returns trigonometric pitch angle in degrees */
+float calculate_pitch(sensors_event_t* accel) {
+  float sq = (accel->acceleration.y * accel->acceleration.y) +
+             (accel->acceleration.z * accel->acceleration.z);
+  float body = -accel->acceleration.x / sqrt(sq);
+  return degrees(atan(body));
+}
+
+/* Returns trigonometric roll angle in degrees */
+float calculate_roll(sensors_event_t* accel) {
+  float sq = (accel->acceleration.x * accel->acceleration.x) +
+             (accel->acceleration.z * accel->acceleration.z);
+  float body = accel->acceleration.y / sqrt(sq);
+  return degrees(atan(body));
+}
+
+// Non-blocking loop using millis() for timing
+unsigned long previousMillis = 0; 
+const long interval = 20;
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    // sensor reads take about 8 milis 
+    sensors_event_t press, alt, temp;
+    bmp.getEvent(&temp, &press, &alt);
+    sensors_event_t accel, gyro, mag, temp2;
+    icm.getEvent(&accel, &gyro, &temp2, &mag);
+
+    gyro.gyro.x -= gyro_bias_x;
+    gyro.gyro.y -= gyro_bias_y;
+    gyro.gyro.z -= gyro_bias_z;
+
+    Serial.print("Tpitch:");
+    Serial.print(calculate_pitch(&accel));
+    Serial.print(" Troll:");
+    Serial.print(calculate_roll(&accel));
+    Serial.print(" Ipitch:");
+    Serial.print(integrate_pitch(&gyro));
+    Serial.print(" Iroll:");
+    Serial.println(integrate_roll(&gyro));
+  }
+}
+
+// void loop() {
+//   // sensor reads take about 8 milis 
+//   sensors_event_t press, alt, temp;
+//   bmp.getEvent(&temp, &press, &alt);
+//   sensors_event_t accel, gyro, mag, temp2;
+//   icm.getEvent(&accel, &gyro, &temp2, &mag);
+
+//   print(&accel, &gyro, &mag, &temp2, &press, &alt);
+//   delay(500); // blocking
+// }
